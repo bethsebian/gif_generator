2d5724ce6a336c9632dd550c5fa8700d6ccc3289
ToniRib <tonimarierib@gmail.com>
2016-01-09 13:10:01 -0700

    user can login to their account

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :gifs, only: [:index, :show]
   resources :users, only: [:new, :create, :show]
 
+  get '/login', to: "sessions#new"
+  post '/login', to: 'sessions#create' 
+
 end
