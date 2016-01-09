20b163216e9a9a1131cda3c63c6a117f786f46c7
ToniRib <tonimarierib@gmail.com>
2016-01-09 15:58:07 -0700

    verifies that user can only see their fav gifs on their gifs page

--- spec/requests/logged_in_user_can_mark_a_gif_as_favorites_spec.rb
+++ spec/requests/logged_in_user_can_mark_a_gif_as_favorites_spec.rb
@@ -16,5 +16,31 @@ RSpec.describe "LoggedInUserCanMarkAGifAsFavorites", type: :request do
       expect(page).to have_content('kitten')
       expect(page).to have_css("img[src*='http://kitten.com']")
     end
+
+    it "only show user's favorites in index" do
+      user_1 = User.create(username: 'user_1', password: 'password', role: 0)
+      user_2 = User.create(username: 'user_2', password: 'password', role: 0)
+
+      ApplicationController.any_instance.stub(:current_user).and_return(user_1)
+
+      category = Category.create(name: 'horse')
+      gif_1 = category.gifs.create(search_term: 'horse', image_url: 'http://horse.com')
+
+      category_2 = Category.create(name: 'kitten')
+      gif_2 = category_2.gifs.create(search_term: 'kitten', image_url: 'http://kitten.com')
+
+      user_2.favorites.create(user_id: user_2.id, gif_id: gif_2.id)
+
+      visit gifs_path
+
+      first(:button, 'Save to Favorites').click
+
+      expect(current_path).to eq(user_favorites_path(user_1))
+      expect(page).to have_content('horse')
+      expect(page).to have_css("img[src*='http://horse.com']")
+
+      expect(page).to_not have_content('kitten')
+      expect(page).to_not have_css("img[src*='http://kitten.com']")
+    end
   end
 end
