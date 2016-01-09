01fbd8eb53aa904778941ace4f50016075650bdd
ToniRib <tonimarierib@gmail.com>
2016-01-09 15:19:47 -0700

    updates tests to use create user instead of new user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   end
 
   resources :gifs, only: [:index, :show, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    resources :favorites, only: [:create]
+  end
 
   get '/login', to: "sessions#new"
   post '/login', to: 'sessions#create'
---
+++ spec/requests/logged_in_user_can_mark_a_gif_as_favorites_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "LoggedInUserCanMarkAGifAsFavorites", type: :request do
+  describe "GET /gifs" do
+    it "let logged in user favorite an image" do
+      user = User.create(username: 'user', password: 'password', role: 0)
+
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+      category = Category.create(name: 'kitten')
+      category.gifs.create(search_term: 'kitten', image_url: 'http://kitten.com')
+
+      visit gifs_path
+      click_on "Save to Favorites"
+
+      expect(current_path).to eq(user_gifs_path)
+      expect(page).to have_content('kitten')
+      expect(page).to have_css("img[src*='http://kitten.com']")
+    end
+  end
+end
