155e9e2474e0386412e0200f4b4649dc7bd367c8
ToniRib <tonimarierib@gmail.com>
2016-01-09 14:16:24 -0700

    category does not get deleted if it has more than one gif

--- spec/requests/only_admin_may_delete_gifs_spec.rb
+++ spec/requests/only_admin_may_delete_gifs_spec.rb
@@ -29,8 +29,20 @@ RSpec.describe "OnlyAdminMayDeleteGifs", type: :request do
       expect(page).to_not have_link("Delete")
     end
 
-    xit "does not delete category if gifs still exist for that category" do
+    it "does not delete category if gifs still exist for that category" do
+      admin = User.new(username: 'admin', password: 'password', role: 1)
 
+      ApplicationController.any_instance.stub(:current_user).and_return(admin)
+      category = Category.create(name: 'kitten')
+      category.gifs.create(search_term: 'kitten', image_url: 'http://kitten.com')
+      category.gifs.create(search_term: 'kitten', image_url: 'http://kittencute.com')
+
+      visit gifs_path
+      first(:link, 'Delete').click
+
+      expect(page).to have_content("kitten")
+      expect(page).to have_css("img[src*='http://kittencute.com']")
+      expect(page).to_not have_css("img[src*='http://kitten.com']")
     end
   end
 
@@ -48,6 +60,18 @@ RSpec.describe "OnlyAdminMayDeleteGifs", type: :request do
       expect(current_path).to eq(gifs_path)
       expect(page).to_not have_content("kitten")
       expect(page).to_not have_css("img")
+    end
+
+    it "does not allow regular user to delete gif from show" do
+      user = User.new(username: 'user', password: 'password', role: 0)
+
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+      category = Category.create(name: 'kitten')
+      gif = category.gifs.create(search_term: 'kitten', image_url: 'http://kitten.com')
+
+      visit gif_path(gif)
+
+      expect(page).to_not have_link("Delete")
     end
   end
 end
